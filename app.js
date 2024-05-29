4c53d4700127dda98d417852762c991961f6e28f
Rybailo <rybailo.orest4@gmail.com>
2024-05-29 23:26:40 +0300

    login, register
    login, register

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import cors from "cors";
 import "dotenv/config";
 
+import authRouter from "./routes/auth.js";
 import contactsRouter from "./routes/contactsRouter.js";
 
 import "./db/db.js";
@@ -13,6 +14,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
+app.use("/auth/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
