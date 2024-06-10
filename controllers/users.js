a7b7df6936d81b33bd78a376c546ad1f42409fd8
Rybailo <rybailo.orest4@gmail.com>
2024-06-10 20:39:36 +0300

    added Jimp
    added Jimp

--- controllers/users.js
+++ controllers/users.js
@@ -1,24 +1,30 @@
 import * as fs from "node:fs/promises";
 import path from "path";
 import User from "../models/users.js";
+import Jimp from "jimp";
 
 async function changeAvatar(req, res, next) {
   try {
-    await fs.rename(
-      req.file.path,
-      path.resolve("public", "avatars", req.file.filename)
-    );
-    const avatarURL = `/avatars/${req.file.filename}`;
+    const avatarPath = req.file.path;
+    const avatarFilename = req.file.filename;
+    const avatarDestination = path.resolve("public", "avatars", avatarFilename);
+    const img = await Jimp.read(avatarPath);
+    await img.resize(250, 250).writeAsync(avatarPath);
+    await fs.rename(avatarPath, avatarDestination);
+
+    const avatarURL = `/avatars/${avatarFilename}`;
     const user = await User.findByIdAndUpdate(
       req.user.id,
       {
-        avatar: req.file.filename,
+        avatar: avatarFilename,
         avatarURL,
       },
       { new: true }
     );
+
     res.status(200).send({ user });
   } catch (error) {
+    console.error("Ошибка при обработке аватарки:", error);
     return res.status(401).send({ message: "Not authorized" });
   }
 }
